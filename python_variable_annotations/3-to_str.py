f2d23aba84aa3e2b3a7ef6c14169b6b71ac41ca5
ghalyahk <ghalyah1166@gmail.com>
2026-02-17 14:15:55 +0000

    Write a type-annotated function

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,5 @@
+#i#usr#bin#evn python3
+"""Module that provides a function to convert a float to a string."""
+def to_str (n: float) -> str:
+ """Returns the string representation of a float."""
+return str(n)
