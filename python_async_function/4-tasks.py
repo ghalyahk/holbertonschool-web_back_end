bd75e5e4793e56cccaadf28e9e22de9bff896a01
ghalyahk <ghalyah1166@gmail.com>
2026-02-19 18:58:36 +0000

    Measure the runtime

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -5,8 +5,8 @@ import asyncio
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list:
-    """Spawns task_wait_random n times"""
+async def task_wait_n(n: int, max_delay: int) -> list[float]:
+    """Spawn n tasks using task_wait_random and return list of delays"""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
 
