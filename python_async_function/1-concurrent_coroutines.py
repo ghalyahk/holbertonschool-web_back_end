6af4e7d6a314b169ac30835b86edf437ea7518ff
ghalyahk <ghalyah1166@gmail.com>
2026-02-19 19:08:00 +0000

    Measure the runtime

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-"""Concurrent coroutines"""
-
+"""
+Module for executing multiple coroutines concurrently
+"""
 import asyncio
 from typing import List
 
@@ -9,13 +10,14 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Spawns wait_random n times with the specified max_delay
-    and returns the list of delays in ascending order.
+    Spawn wait_random n times with the specified max_delay
+
+    Args:
+        n: Number of times to spawn wait_random
+        max_delay: Maximum delay for wait_random
+
+    Returns:
+        List of all delays in ascending order
     """
-    delays = []
-
-    for _ in range(n):
-        delay = await wait_random(max_delay)
-        delays.append(delay)
-
-    return delays
+    delays = await asyncio.gather(*[wait_random(max_delay) for _ in range(n)])
+    return sorted(delays)
