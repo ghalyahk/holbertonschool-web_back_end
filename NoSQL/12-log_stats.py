e495e6d8a26947bbc522823858c91f30b1bc5b0d
ghalyahk <ghalyah1166@gmail.com>
2026-03-07 03:50:06 +0000

    Write a script that lists all databases in MongoDB.

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
-"""12-log_stats.py"""
+"""Log stats"""
 from pymongo import MongoClient
 
+
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient("mongodb://127.0.0.1:27017")
     collection = client.logs.nginx
 
-    total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
-
+    print(f"{collection.count_documents({})} logs")
     print("Methods:")
-    for method in ("GET", "POST", "PUT", "PATCH", "DELETE"):
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
 
-    status_check = collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_check} status check")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        print("\tmethod {}: {}".format(
+            method,
+            collection.count_documents({"method": method})
+        ))
+
+    print("{} status check".format(
+        collection.count_documents({"method": "GET", "path": "/status"})
+    ))
