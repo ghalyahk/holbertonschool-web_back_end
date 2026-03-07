2f67de6072eb426bb646ab3c15af10dbc8ff36f0
ghalyahk <ghalyah1166@gmail.com>
2026-03-07 03:55:42 +0000

    Simple pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""Module to calculate start and end indices for pagination."""
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Returns a tuple containing the start\
+        and end indices for pagination."""
+    first_index = (page - 1) * page_size
+    last_index = first_index + page_size
+    return first_index, last_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns a list of rows from the dataset\
+            for the specified page and page size."""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start_index, end_index = index_range(page, page_size)
+        data = self.dataset()
+
+        if start_index >= len(data):
+            return []
+
+        return data[start_index:end_index]
