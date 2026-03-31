5da67cc8d843087f7eafbf22a942bea07f129009
ghalyahk <ghalyah1166@gmail.com>
2026-03-31 15:50:21 +0000

    Basic list of objects

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,9 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, photoPromise]);
+}
