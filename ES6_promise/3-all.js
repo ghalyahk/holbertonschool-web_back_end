9be01e0f1dbdcca86f424c26ef334f768ca43938
ghalyahk <ghalyah1166@gmail.com>
2026-03-31 15:43:58 +0000

    Basic list of objects

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       const photoResult = results[0]; // uploadPhoto result
-      console.log(`${photoResult.body.firstName} ${photoResult.body.lastName}`);
+      console.log(photoResult); // log it directly
     })
     .catch(() => {
       console.log('Signup system offline');
