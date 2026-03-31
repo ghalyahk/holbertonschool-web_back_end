154236c3c4098091f806c31459ec110d5e9194d7
ghalyahk <ghalyah1166@gmail.com>
2026-03-31 15:53:25 +0000

    Basic list of objects

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,5 +5,13 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([userPromise, photoPromise]);
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) =>
+      results.map((r) => {
+        if (r.status === 'fulfilled') {
+          return { status: r.status, value: r.value };
+        }
+        return { status: 'rejected', value: r.reason }; // map reason -> value
+      })
+    );
 }
