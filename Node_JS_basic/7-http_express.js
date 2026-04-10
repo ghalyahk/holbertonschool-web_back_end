5e483fabc8ab433d0c51de5655e16c183ca59038
ghalyahk <ghalyah116@gmai.com>
2026-04-10 17:29:35 +0000

    holbertonschool

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,24 +1,19 @@
 const express = require('express');
 const fs = require('fs');
 
-const app = express();
-
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
+    fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
         reject(new Error('Cannot load the database'));
         return;
       }
 
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const lines = data.split(/\r?\n/).filter((line) => line.trim() !== '');
       const students = lines.slice(1);
-
-      console.log(`Number of students: ${students.length}`);
-
       const fields = {};
 
-      students.forEach((line) => {
+      for (const line of students) {
         const parts = line.split(',');
         const firstname = parts[0];
         const field = parts[3];
@@ -28,37 +23,37 @@ function countStudents(path) {
         }
 
         fields[field].push(firstname);
-      });
+      }
+
+      let result = `Number of students: ${students.length}`;
 
       Object.keys(fields).forEach((field) => {
-        console.log(
-          `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
-        );
+        result += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
       });
 
-      resolve();
+      resolve(result);
     });
   });
 }
 
-// /
+const app = express();
+
 app.get('/', (req, res) => {
+  res.type('text/plain');
   res.send('Hello Holberton School!');
 });
 
-// /students
 app.get('/students', (req, res) => {
-  const db = process.argv[2];
+  const database = process.argv[2];
 
-  res.write('This is the list of our students\n');
-
-  countStudents(db)
-    .then(() => {
-      res.end();
+  countStudents(database)
+    .then((result) => {
+      res.type('text/plain');
+      res.send(`This is the list of our students\n${result}`);
     })
     .catch((err) => {
-      res.write(`${err.message}`);
-      res.end();
+      res.type('text/plain');
+      res.send(`This is the list of our students\n${err.message}`);
     });
 });
 
